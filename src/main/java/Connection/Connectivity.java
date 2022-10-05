faad464555fef5828929760dcf3e2ddac08d0eeb
Tuan le <leminhtuan48198@gmail.com>
2022-10-05 15:24:33 +0700

    add

---
+++ src/main/java/Connection/Connectivity.java
@@ -0,0 +1,26 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connectivity {
+    private String jdbcURL = "jdbc:mysql://localhost:3306/demo1?";
+    private String jdbcUsername = "root";
+    private String jdbcPassword = "123456";
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
